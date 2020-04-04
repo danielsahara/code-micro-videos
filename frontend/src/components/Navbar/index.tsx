917717a18934cc84f5cd4e5bc72ac389aec83eae
Daniel Sahara <d.otasahara@gmail.com>
2020-04-04 15:30:15 -0300

    Estilizando navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
-import {AppBar, Toolbar} from "@material-ui/core";
+import {AppBar, Button, Toolbar, Typography} from "@material-ui/core";
+import logo from '../../static/img/logo.png'
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    toolbar: {
+        backgroundColor: "#000000"
+    },
+    title:{
+        flexGrow: 1,
+        textAlign: 'center'
+    }
+})
 
 export const Navbar: React.FC = () => {
+    const classes = useStyles();
+    console.log(classes);
+
     return (
         <AppBar>
-            <Toolbar>
-                texto
+            <Toolbar className={classes.toolbar}>
+                <Typography className={classes.title}>
+                    <img src={logo} alt="CodeFlix" />
+                </Typography>
+                <Button color="inherit">Login</Button>
             </Toolbar>
         </AppBar>
     );
