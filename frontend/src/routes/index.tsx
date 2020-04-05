502a446b20c9371f0748ccb3e541dd98c89eaf50
Daniel Sahara <d.otasahara@gmail.com>
2020-04-05 19:12:45 -0300

    Listando rotas com menu de usuario

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,18 +2,21 @@ import {RouteProps} from 'react-router-dom';
 import Dashboard from "../pages/Dashboard";
 import CategoryList from "../pages/category/List";
 
-interface MyRouteProprs extends RouteProps{
+export interface MyRouteProps extends RouteProps{
+    name: string;
     label: string;
 }
 
-const routes : MyRouteProprs[] = [
+const routes : MyRouteProps[] = [
     {
+        name: 'dashboard',
         label: 'Dashboard',
         path: '/',
         component: Dashboard,
         exact: true
     },
     {
+        name: 'categories.list',
         label: 'Listar categorias',
         path: '/categories',
         component: CategoryList,
