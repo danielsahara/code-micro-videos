55fc6b9de66e413f870bfcd3bc0d75f9782c389a
Daniel Sahara <d.otasahara@gmail.com>
2020-04-05 12:21:52 -0300

    Criando formato de pagina

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css'
 import {Navbar} from "./components/Navbar";
+import {Page} from "./components/Page";
+import {Box} from "@material-ui/core";
 
 const App: React.FC = () => {
   return (
-      <Navbar />
+      <React.Fragment>
+        <Navbar />
+        <Box paddingTop={'70px'}>
+
+        </Box>
+        <Page title={'Categorias'}>
+          Conteudo
+        </Page>
+      </React.Fragment>
   );
 };
 
