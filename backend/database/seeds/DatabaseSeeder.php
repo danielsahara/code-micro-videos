670ebedb81b22a75628da3376ba7464994012c9c
Daniel Sahara <d.otasahara@gmail.com>
2020-04-08 09:16:00 -0300

    projeto listagens

--- backend/database/seeds/DatabaseSeeder.php
+++ backend/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call(CategoriesTableSeeder::class);
          $this->call(GenresTableSeeder::class);
          $this->call(VideoSeeder::class);
+         $this->call(CastMemberSeeder::class);
     }
 }
