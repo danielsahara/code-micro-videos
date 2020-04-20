56cfcfed27944faf9c052b97f9f0c5d2a162c047
Daniel Sahara <d.otasahara@gmail.com>
2020-04-19 22:12:22 -0300

    Iniciando com filtros personalizados

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\ModelFilters\CategoryFilter;
+use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes, Traits\Uuid;
+    use SoftDeletes, Traits\Uuid, Filterable;
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [
@@ -19,5 +21,9 @@ class Category extends Model
 
     public function genres(){
         return $this->belongsToMany(Genre::class);
+    }
+
+    public function modelFilter(){
+        return $this->provideFilter(CategoryFilter::class);
     }
 }
