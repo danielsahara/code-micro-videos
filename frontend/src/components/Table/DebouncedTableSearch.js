d5a7080dbd4b8efc8b977e3b5d9190e08347f8f5
Daniel Sahara <d.otasahara@gmail.com>
2020-04-27 10:09:11 -0300

    Melhorias no componente personalizado

--- frontend/src/components/Table/DebouncedTableSearch.js
+++ frontend/src/components/Table/DebouncedTableSearch.js
@@ -27,25 +27,52 @@ const defaultSearchStyles = theme => ({
     },
 });
 
-class DebouncedTableSearch extends React.Component {
+class DebouncedTableSearch extends React.PureComponent {
 
     constructor(props) {
         super(props);
+        const {searchText} = this.props;
+        let value = searchText;
+
+        if(searchText && searchText.value !== undefined){
+            value = searchText.value;
+        }
         this.state = {
             text: props.searchText
         }
-        this.dispatchOnSearch = debounce(this.dispatchOnSearch.bind(this), this.props.debounceTime);
+        this.debouncedOnSearch = debounce(this.debouncedOnSearch.bind(this), this.props.debounceTime);
     }
 
     handleTextChange = event => {
         const value = event.target.value;
         this.setState({
             text: value
-        }, () => this.dispatchOnSearch(value))
+        }, () => this.debouncedOnSearch(value))
     };
 
-    dispatchOnSearch = value => {
+    debouncedOnSearch = value => {
         this.props.onSearch(value)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const {searchText } = this.props;
+
+        if(searchText && searchText.value !== undefined && prevProps.searchText !== this.props.searchText){
+            const value = searchText.value;
+            if(value){
+                this.setState({
+                    text: value
+                }, () => this.props.onSearch(value));
+            }
+            else{
+                try {
+                    this.props.onHide()
+                }
+                catch (e) {
+
+                }
+            }
+        }
     }
 
     componentDidMount() {
@@ -65,10 +92,6 @@ class DebouncedTableSearch extends React.Component {
     render() {
         const { classes, options, onHide, searchText } = this.props;
         let value = this.state.text;
-
-        if(searchText && searchText.value !== undefined){
-            value = searchText.value;
-        }
 
         return (
             <Grow appear in={true} timeout={300}>
