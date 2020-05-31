fe2c2e8e065e8a6a0334ff2c8d1ebdd23111acb3
Daniel Sahara <d.otasahara@gmail.com>
2020-05-31 12:40:24 -0300

    Criando Spinner para Requisições Ajax

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import AppRouter from "./routes/AppRouter";
 import Breadcrumbs from "./components/Breadcrumbs";
 import theme from "./theme";
 import {SnackbarProvider} from "./components/SnackbarProvider";
+import Spinner from "./components/Spinner";
 
 const App: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const App: React.FC = () => {
               <CssBaseline />
               <SnackbarProvider>
                   <BrowserRouter>
+                      <Spinner/>
                       <Navbar />
                       <Box paddingTop={'70px'}>
                           <Breadcrumbs />
