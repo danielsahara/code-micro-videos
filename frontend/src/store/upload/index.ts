fce923f6f7ae1942f1ba0551af03e9669a30ef39
Daniel Sahara <d.otasahara@gmail.com>
2020-06-10 20:38:42 -0300

    Criando ação para adicionar arquivos na dila de upload

---
+++ frontend/src/store/upload/index.ts
@@ -0,0 +1,28 @@
+import * as Typings from "./types";
+import {createActions, createReducer} from 'reduxsauce';
+
+export const {Types, Creators} = createActions<{
+    ADD_UPLOAD: string,
+    },{
+    addUpload(payload: Typings.AddUploadAction['payload']): Typings.AddUploadAction
+
+}>
+({
+    addUpload: ['payload'],
+});
+
+export const INITIAL_STATE : Typings.State = {
+    uploads: []
+};
+
+const reducer = createReducer<Typings.State, Typings.Actions>(INITIAL_STATE, {
+    [Types.ADD_UPLOAD] : addUpload as any,
+});
+
+export default reducer;
+
+function addUpload(state = INITIAL_STATE, action: Typings.AddUploadAction) : Typings.State{
+    return{
+
+    };
+}
--- frontend/src/store/upload/types.ts
+++ frontend/src/store/upload/types.ts
@@ -1,5 +1,6 @@
 import {Video} from "../../util/models";
 import {AxiosError} from "axios";
+import {AnyAction} from "redux";
 
 export interface FileUpload{
     fileField: string;
@@ -14,6 +15,15 @@ export interface Upload{
     files: FileUpload[]
 }
 
-export interface UploadCollection {
+export interface State {
     uploads: Upload[]
 }
+
+export interface AddUploadAction extends AnyAction{
+    payload: {
+        video: Video,
+        files: Array<{file: File, fileField: string}>
+    }
+}
+
+export type Actions = AddUploadAction;
