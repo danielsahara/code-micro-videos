561887d72109193ab0b48074c1a97725d95172af
Daniel Sahara <d.otasahara@gmail.com>
2020-06-14 00:23:57 -0300

    Iniciando com Redux Saga Effects

--- frontend/redux-saga-test.js
+++ frontend/redux-saga-test.js
@@ -1,25 +1,40 @@
 const {createStore, applyMiddleware} = require('redux');
 const {default: createSagaMiddleware} = require('redux-saga');
+const {take, put} = require('redux-saga/effects');
 
 function reducer(state, action) {
-    return {value: action.value};
+    if (action.type === 'acaoX') {
+        return {value: action.value};
+    }
+    return state;
 }
 
 function* helloWorldSaga() {
-    console.log("hellow")
+    console.log("hellow");
+
+    const action = yield take('acaoY');
+
+    //logica ---------------
+
+    const result = yield put({
+        type: 'acaoX',
+        value: 'novo valor',
+
+    });
+    // console.log(result);
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     reducer,
-        applyMiddleware(sagaMiddleware)
+    applyMiddleware(sagaMiddleware)
 );
 
 sagaMiddleware.run(helloWorldSaga);
 
 const action = (type, value) => store.dispatch({type, value});
 
-action('AcaoX', 'a');
+action('acaoY', 'a');
 
 console.log(store.getState())
