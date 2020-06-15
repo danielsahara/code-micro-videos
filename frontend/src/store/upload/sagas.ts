3981288911e7e89a312e9a551eda1e2ffd78e1df
Daniel Sahara <d.otasahara@gmail.com>
2020-06-15 07:39:40 -0300

    Capturando novos arquivos de upload com Saga

--- frontend/src/store/upload/sagas.ts
+++ frontend/src/store/upload/sagas.ts
@@ -1,4 +1,13 @@
+import {Types} from './index';
+import {actionChannel, take} from 'redux-saga/effects'
+import {AddUploadAction} from "./types";
 
 export function* uploadWatcherSaga() {
 
+    const newFilesChannel = yield actionChannel(Types.ADD_UPLOAD);
+
+    while (true){
+        const {payload}:AddUploadAction = yield take(newFilesChannel);
+        console.log(payload);
+    }
 }
