5a5bbb145b986523d5fce140839d2498601a2dc7
Daniel Sahara <d.otasahara@gmail.com>
2020-06-16 08:22:22 -0300

    Mostrando quantos uploads estão ativos no momento

---
+++ frontend/src/store/upload/getters.ts
@@ -0,0 +1,8 @@
+import {Upload} from "./types";
+
+export function countInProgress(uploads: Upload[]) {
+    return uploads.filter(upload => !isFinished(upload)).length;
+}
+export function isFinished(upload: Upload) {
+    return upload.progress === 1;
+}
